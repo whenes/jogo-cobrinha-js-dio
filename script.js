252c22392abbe407135bc6824cba88e115d3b9b0
whenes <whenes.oliveira@gmail.com>
2020-09-03 20:15:31 -0300

    Adicionado movimentação para o personagem do jogo.

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake[0] = { 
-  x: 8 * box,
-  y: 8 * box
-}
+let direction = "right";
+
+snake[0] = { x: 8 * box, y: 8 * box }
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -19,5 +18,23 @@ function criarCobrinha() {
   }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction === "right") { snakeX += box; }
+  if (direction === "left") { snakeX -= box; }
+  if (direction === "up") { snakeY -= box; }
+  if (direction === "down") { snakeY += box; }
+
+  snake.pop();
+
+  let newHead = { x: snakeX, y: snakeY}
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
